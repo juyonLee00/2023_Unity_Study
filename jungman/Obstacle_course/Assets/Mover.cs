41793921741b3f8a8f5dee31dd4942031dc69b88
munjungmin <answjdals77@gmail.com>
2023-01-10 10:05:49 +0900

    Variable 소개

--- jungman/Obstacle_course/Assets/Mover.cs
+++ jungman/Obstacle_course/Assets/Mover.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float xValue = 0;
+    float yValue = 0.01f;
+    float zValue = 0;
+
     void Start()
-    {
+    {    
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
+
+
+// 변수로 값을 관리할 수 있음 
