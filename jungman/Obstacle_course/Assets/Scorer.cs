ae778be99270b9c34d026dd36548d8ea52275ea5
munjungmin <answjdals77@gmail.com>
2023-01-10 23:19:39 +0900

    Score 증가

--- jungman/Obstacle_course/Assets/Scorer.cs
+++ jungman/Obstacle_course/Assets/Scorer.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectHit : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
+    int hits = 0;
     private void OnCollisionEnter(Collision other) 
     {
-        GetComponent<MeshRenderer>().material.color = Color.blue;
+        hits++;
+        Debug.Log("You've bumped into a thing this many times: " + hits);
     }
 }
-
