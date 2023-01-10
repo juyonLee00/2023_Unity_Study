9d0776903b06f9448f326f65ea6f00a2919a7360
munjungmin <answjdals77@gmail.com>
2023-01-10 20:16:50 +0900

    Method 실습하기

--- jungman/Obstacle_course/Assets/Mover.cs
+++ jungman/Obstacle_course/Assets/Mover.cs
@@ -7,26 +7,32 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 7.0f;
     void Start()
     {    
-
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("You can control your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed; 
-        transform.Translate(xValue, 0, zValue);
+
+        transform.Translate(xValue, 0, zValue); 
     }
 }
 
 
 /*
-개체들끼리 서로 부딪히려면 rigid body라는 것을 추가해야 함 
-rigid body - 개체에 질량 부여 
-
-부딪혀서 뒤로 밀어냄 (작용-반작용?)
-Freeze Position / Rotation 설정 
-
-구석에 틈이 좀 있으면 벽을 뚫고 나갈 수 있음 - 나중에 ㄱ 
-(# 모양으로 만들었는데도 구석에서 뚫고 나가네..뭐지)
+변수는 소문자 / 메소드는 대문자로 시작
 */
