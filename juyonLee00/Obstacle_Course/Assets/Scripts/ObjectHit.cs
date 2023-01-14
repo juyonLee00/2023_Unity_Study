03766cf545e9d7ab9e726555a683d790b12b7ed7
juyonLee00 <ljy012900@gmail.com>
2023-01-14 19:45:39 +0900

    Prefab 추가 및 Component 정리

--- juyonLee00/Obstacle_Course/Assets/Scripts/ObjectHit.cs
+++ juyonLee00/Obstacle_Course/Assets/Scripts/ObjectHit.cs
@@ -6,6 +6,12 @@ public class ObjectHit : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other) 
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        //전달된 다른 개체의 정보
+        if(other.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            //스크립트가 부착된 게임 개체의 정보
+            gameObject.tag = "Hit";
+        }
     }
 }
--- juyonLee00/Obstacle_Course/Assets/Scripts/Scorer.cs
+++ juyonLee00/Obstacle_Course/Assets/Scripts/Scorer.cs
@@ -8,6 +8,10 @@ public class Scorer : MonoBehaviour
 
    private void OnCollisionEnter(Collision other) 
    {
-    hits++;
+        if(other.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You've bumped into a thing this many times: " + hits);
+        }
    }
 }
