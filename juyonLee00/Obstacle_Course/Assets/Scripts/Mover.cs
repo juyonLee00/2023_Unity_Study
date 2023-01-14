fa315a72ef250fab59f6084022ab8d14032e2f8c
juyonLee00 <ljy012900@gmail.com>
2023-01-14 15:46:56 +0900

    Input값에 따른 플레이어 이동 설정

--- juyonLee00/Obstacle_Course/Assets/Scripts/Mover.cs
+++ juyonLee00/Obstacle_Course/Assets/Scripts/Mover.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
-    //Inspector에서 변수값 실시간 변경 가능. 그러나 Inspector변수값이 실제 변수값에 오버라이딩 되지는 않음.
-    [SerializeField] float xValue;
-    [SerializeField] float yValue;
-    [SerializeField] float zValue;
+    //SerializeField Inspector에서 변수값 실시간 변경 가능. 그러나 Inspector변수값이 실제 변수값에 오버라이딩 되지는 않음.
+    
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +17,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Input Manager의 축값을 가져와 키에 따라 -1, +1값 적용.
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+
+        transform.Translate(xValue, 0, zValue);
     }
 }
