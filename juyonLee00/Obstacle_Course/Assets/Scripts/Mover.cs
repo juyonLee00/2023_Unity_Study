dbf1ada14390cc5dfea993d76e5b4614081b6633
juyonLee00 <ljy012900@gmail.com>
2023-01-14 15:56:27 +0900

    Time.deltaTime을 사용해 플레이어 움직임을 frame rate로부터 독립

--- juyonLee00/Obstacle_Course/Assets/Scripts/Mover.cs
+++ juyonLee00/Obstacle_Course/Assets/Scripts/Mover.cs
@@ -6,7 +6,7 @@ public class Mover : MonoBehaviour
 {
 
     //SerializeField Inspector에서 변수값 실시간 변경 가능. 그러나 Inspector변수값이 실제 변수값에 오버라이딩 되지는 않음.
-    
+    [SerializeField] float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +18,13 @@ public class Mover : MonoBehaviour
     void Update()
     {
         //Input Manager의 축값을 가져와 키에 따라 -1, +1값 적용.
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        //Time.deltaTime: 프레임 실행 시 걸리는 시간을 알려줌(CPU에 따른 차이 존재)
+        //Frames per second * Distance per Second을 통해 frame rate로부터 독립.
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical")*Time.deltaTime * moveSpeed;
 
         transform.Translate(xValue, 0, zValue);
+
+        
     }
 }
